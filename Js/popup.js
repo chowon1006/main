75d3092c57f21f910874ccc1b480b976cc40419a
chowon <a39402819@gmail.com>
2025-04-16 13:52:38 +0900

    7주차차

--- Js/popup.js
+++ Js/popup.js
@@ -1,3 +1,21 @@
+// var close_time; // 시간 정보
+// var close_time2 = 10; // 10초 설정
+//     clearTimeout(close_time); // 재호출 정지
+//     close_time= setTimeout("close_window()", 10000);
+//     // 1/1000 초 지정, 바로 시작 
+//     show_time(); // 실시간 시간 보여주기
+
+//     <h1 class="display-4"><div id="Time" class="clock"></div></h1>
+//     function show_time(){
+//     let divClock = document.getElementById('Time');
+//     divClock.innerText = close_time2; // 10초 삽입 시작
+//     close_time2--; // 1초씩 감소
+//     setTimeout(show_time, 1000);  //1초마다 갱신
+// }
+//     function close_window() { // 함수 정의
+//     window.close(); // 윈도우 닫기
+// }
+
 function pop_up()
     {
     window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
@@ -5,6 +23,7 @@ function pop_up()
 
     function show_clock()
     {
+
         let currentDate= new Date(); // 현재시스템날짜객체생성
         let divClock= document.getElementById('divClock');
         let msg = "현재시간: ";
@@ -36,3 +55,11 @@ function pop_up()
         function out(obj) {
         obj.src="image/steam.png";
         }
+
+        // const over = (obj) => {
+        //     obj.src = "image/main.png";
+        //     };
+        //     const search_message = () => {
+        //     const c = '검색을 수행합니다';
+        //     alert(c);
+        //     };
